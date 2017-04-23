4f8e1ef5939992ac94e7f5ea0a37f409cf420c80
Hamza Khatri <hamzakhatri1@hotmail.com>
2017-04-22 19:43:53 -0500

    added client, server packages

---
+++ src/client/ClientMain.java
@@ -0,0 +1,19 @@
+package client;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClientMain implements Observer {
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/server/ServerMain.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.util.Observable;
+
+public class ServerMain extends Observable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
