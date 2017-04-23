2d6b0f1c925442454d0ea8f2647bbf0184a5040f
Hamza Khatri <hamzakhatri1@hotmail.com>
2017-04-22 23:02:12 -0500

    jj

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -6,9 +6,9 @@ import java.util.Observer;
 public class ClientMain implements Observer {
 
 	@Override
-	public void update(Observable arg0, Object arg1) {
+	public void update(Observable server, Object obj) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	public static void main(String[] args) {
